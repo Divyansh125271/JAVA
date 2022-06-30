37e66dd5f1ce3735656cabf8b46f52eb42087e79
Divyansh125271 <108382617+Divyansh125271@users.noreply.github.com>
2022-06-30 16:48:14 +0530

    This program is for Factorial of a number.

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Factorial{
+public static void main(String args[]){
+int a = 5;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the value of a:");
+a = scanner.nextInt();
+int factorial = 1 ;
+  for(int i = 1 ; i <= a ; i++){
+  factorial = factorial*i ;
+System.out.println("Factorial is "+factorial);  
+ }
+
+}
+}
+
