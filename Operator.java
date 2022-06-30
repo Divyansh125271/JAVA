ef2d744157c9f4f92c7bce765b21073ce0592753
Divyansh125271 <108382617+Divyansh125271@users.noreply.github.com>
2022-06-30 17:16:08 +0530

    This program contain arithmetic operator using switch statement.

---
+++ Operator.java
@@ -0,0 +1,46 @@
+class Opeartor{
+
+public static void main(String args[]){
+
+int a;
+
+int b;
+
+String op ;
+
+Scanner scanner = new scanner(System.in);
+
+System.out.println("Enter the value of a");
+
+
+a = scanner.nextInt();
+
+System.out.println("Enter the value of b");
+
+b = scanner.nextInt();
+
+System.out.println("Enter the operator +,-,*,/");
+
+op = scanner.next();
+
+
+switch(op){
+
+case "+": System.out.println(a+b); 
+break;
+
+case "-": System.out.println(a-b);
+ break;
+
+case "*": System.out.println(a*b);
+ break;
+
+case "/": System.out.println(a/b); 
+break;
+
+default: System.out.println("Invalid input");
+}
+
+}
+
+}
